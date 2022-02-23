45baf7e79acab12e8d7448ecf9434581f5d2a82f
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-23 12:34:58 +0530

    ADDED: Inserting Element/data at end

--- src/main/java/com/bridgeLabz/linkedList/LinkedList.java
+++ src/main/java/com/bridgeLabz/linkedList/LinkedList.java
@@ -25,7 +25,18 @@ public class LinkedList {
                 System.out.print(temp.data+ " ");
                 temp = temp.next;
             }
-
         }
     }
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
 }
