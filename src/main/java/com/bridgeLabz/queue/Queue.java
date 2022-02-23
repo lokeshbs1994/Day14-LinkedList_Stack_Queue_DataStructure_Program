63800aada5dfba1cb7b2cc9ace3cedb2a9cc4b05
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-23 17:00:00 +0530

    ADDED: Ability to Dequeue from begining

--- src/main/java/com/bridgeLabz/queue/Queue.java
+++ src/main/java/com/bridgeLabz/queue/Queue.java
@@ -6,15 +6,28 @@ import com.bridgeLabz.linkedList.Node;
 public class Queue {
     private Node top;
     private LinkedList linkedList;
+    Node head;
+    Node tail;
 
     public Queue() {
         this.linkedList = new LinkedList();
     }
 
     public void enqueue() {
-        linkedList.append(56);
+        head = linkedList.append(56);
         linkedList.append(30);
         linkedList.append(70);
+        linkedList.print();
+    }
+    public void dequeue() {
+        enqueue();
+        top = head;
+        Node temp = top;
+        while (temp != null) {
+            linkedList.pop();
+            temp = temp.next;
+        }
+        System.out.println("After");
         linkedList.print();
     }
 }
